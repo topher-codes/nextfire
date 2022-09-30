08e7ac8637e1d56429d16a3499b81560958170df
Chris <ghost@drednaut.net>
2022-09-30 10:27:47 -0700

    added firebase boilerplate
    Signed-off-by: Chris <ghost@drednaut.net>

---
+++ lib /firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyDsPPEBHu6G-xQeM-nP5jZyPNwANn96s34',
+	authDomain: 'fir-project-48d1a.firebaseapp.com',
+	projectId: 'fir-project-48d1a',
+	storageBucket: 'fir-project-48d1a.appspot.com',
+	messagingSenderId: '812587000254',
+	appId: '1:812587000254:web:eab5e6eaaf000f51d659ef',
+	measurementId: 'G-WH6G0CVZLT',
+};
+
+if (!firebase.apps.length) {
+	firebase.initializeApp(firebaseConfig);
+}
