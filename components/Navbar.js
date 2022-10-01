6874f92fc86025d58654ffb52b5e35a17f23a1dd
Chris <ghost@drednaut.net>
2022-10-01 10:30:16 -0700

    added navbar links and styles
    Signed-off-by: Chris <ghost@drednaut.net>

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,6 +10,32 @@ export default function Navbar() {
 						<button>FEED</button>
 					</Link>
 				</li>
+
+				{/* user is signed-in and has username */}
+				{username && (
+					<>
+						<li>
+							<Link href={'/admin'}>
+								<button className="btn-blue">Write Posts</button>
+							</Link>
+						</li>
+
+						<li>
+							<Link href={`/${username}`}>
+								<img src={user?.photoURL || '/hacker.png'} />
+							</Link>
+						</li>
+					</>
+				)}
+
+				{/* user is not signed OR has not created username */}
+				{!username && (
+					<li>
+						<Link href={'/enter'}>
+							<button className="btn-blue">Log in</button>
+						</Link>
+					</li>
+				)}
 			</ul>
 		</nav>
 	);
